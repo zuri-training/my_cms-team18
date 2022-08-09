8697df2abd071a7c23c3b286df4afba3e3bd777c
Emmanuel Hillary <obawoledavid@gmail.com>
2022-08-09 10:22:46 +0100

    updated new templates

--- WebpageBuilder/settings.py
+++ WebpageBuilder/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rj7fd3do=ba@_x=&ix=343k7g7#3nvr4d3r7)q(do*@ns$)1#x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
